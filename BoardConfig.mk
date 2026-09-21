06266e23b9a9992286de1f2e98705033af357bdf
Alin Jerpelea <alin.jerpelea@sony.com>
2026-09-21 12:49:32 +0200

    initial config for Xperia 1 VI (PDX245)
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,50 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Platform
+PRODUCT_PLATFORM := asahi
+
+include device/sony/asahi/PlatformConfig.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := unknown
+ifneq (,$(filter %xqec54,$(TARGET_PRODUCT)))
+TARGET_BOOTLOADER_BOARD_NAME := XQ-EC54
+else
+TARGET_BOOTLOADER_BOARD_NAME := XQ-EC54
+$(warning Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)", using default value: "$(TARGET_BOOTLOADER_BOARD_NAME)")
+endif
+
+# Kernel cmdline
+BOARD_BOOTCONFIG += androidboot.hardware=pdx245
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=pdx245
+
+# FB console
+#BOARD_KERNEL_CMDLINE += earlycon=simplefb,0xb8000000,1096,2560
+
+# Partition information
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x06000000
+BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE := 0x00800000
+BOARD_DTBOIMG_PARTITION_SIZE := 0x01800000
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 0x06000000
+
+BOARD_SUPER_PARTITION_SIZE := 14495514624
+
+# Set error limit to SUPER_PARTITION_SIZE - 500MiB
+BOARD_SUPER_PARTITION_ERROR_LIMIT := 13971226624
+
+# DYNAMIC_PARTITIONS_SIZE = (SUPER_PARTITION_SIZE / 2) - 4MiB
+BOARD_SOD_DYNAMIC_PARTITIONS_SIZE := 7243563008
+
+# Reserve space for data encryption (0x35E7AA5000-0x4000)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 231519948800
---
+++ device.mk
@@ -0,0 +1,91 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Device path
+DEVICE_PATH := device/sony/pdx245/rootdir
+
+# Device Specific Permissions
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Audio Configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
+    $(DEVICE_PATH)/vendor/etc/mixer_paths_pineapple_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pineapple_qrd.xml
+
+# Audio calibration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/acdbdata/pineapple_qrd/acdb_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/pineapple_qrd/acdb_cal.acdb \
+    $(DEVICE_PATH)/vendor/etc/acdbdata/pineapple_qrd/workspaceFileXml.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/pineapple_qrd/workspaceFileXml.qwsp
+
+# NFC Configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# Touch firmware
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/firmware/touch_module_id_0x01.img:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_module_id_0x03.img
+
+# Audio calibration
+PRODUCT_COPY_FILES += \
+
+# Device-specific magnetometer and IMU orientation
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/sns_device_orient_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sns_device_orient_somc_platform.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_lsm6dso_0_somc_product.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_lsm6dso_0_somc_product.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_somc_default_sensors.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_somc_default_sensors.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_bmp5_0_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_bmp5_0_somc_platform.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_ak991x_0_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_ak991x_0_somc_platform.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_lsm6dso_0_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_lsm6dso_0_somc_platform.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_stk3acx_0_somc_platform.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_stk3acx_0_somc_platform.json \
+    $(DEVICE_PATH)/vendor/etc/sensors/config/lanai_ak991x_0_somc_product.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/lanai_ak991x_0_somc_product.json
+
+# Device Runtime Resource Overlays
+PRODUCT_PACKAGES += \
+    FrameworkOverlayPDX245 \
+    SystemUIOverlayPDX245
+
+# Device Init
+PRODUCT_PACKAGES += \
+    fstab.pdx245 \
+    ramdisk-fstab.pdx245 \
+    init.recovery.pdx245 \
+    init.pdx245
+
+# Telephony Packages (AOSP)
+PRODUCT_PACKAGES += \
+    InCallUI \
+    Stk
+
+# SAR
+PRODUCT_PACKAGES += \
+    TransPowerSensors
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=420 \
+    ro.vendor.usb.pid_suffix=20d
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/asahi/platform.mk)
+
+# copy NFC firmware
+$(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
+
+# include board vendor blobs
+$(call inherit-product-if-exists, vendor/sony/asahi-common/asahi-partial.mk)
